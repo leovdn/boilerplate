0fe1ba939817e40d1c743fce4a0e62b988c266b2
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-03-19 09:23:40 -0300

    chore: install and config styled-components with next

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Boilerplate</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="favicon.ico" type="image/x-icon" />
+        <meta name="description" content="Boilerplate for React, Next and TS" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
