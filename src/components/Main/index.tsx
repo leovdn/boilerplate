49b9584f62999f2051764bdb272c6a61299ce9d5
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2024-03-19 15:16:07 -0300

    feat: create component styles and add images

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,21 @@
+import * as S from './styles'
+
 const Main = () => {
   return (
-    <main>
-      <h1>Main</h1>
-    </main>
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado"
+      />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        TypeScript, ReactJS, Next.Js, Styled Components
+      </S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor com telas ao lado"
+      ></S.Illustration>
+    </S.Wrapper>
   )
 }
 
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -6,8 +6,16 @@ describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />)
 
-    expect(screen.getByRole('heading', { name: /Main/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /React Avançado/i })
+    ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render color correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
